1e268b54e64c73a647ab0f0b6fe5a8dcefcb25b1
Marisabel Trejo <97261405+MarisabelTrejo@users.noreply.github.com>
2025-01-18 07:54:41 -0600

    changes

--- server.js
+++ server.js
@@ -1,45 +1,26 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-console.log('express-ejs-layouts loaded successfully');
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
-
-/* ***********************
- * View Engine and Templates
- *************************/
 app.set("view engine", "ejs")
-app.set(expressLayouts)
-app.set("partials")
+app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
-/* ***********************
- * Routes
- *************************/
+app.set("views", "./views")
+
+app.use(express.static("public"))
 app.use(static)
 
-// Index Route
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
+
+
 const port = process.env.PORT
 const host = process.env.HOST
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
